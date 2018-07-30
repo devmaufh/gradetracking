48a66438e2d9b4bc73db2687aa85119c6f57918f
Mauricio Flores Hernández <mau1361317@gmail.com>
2018-07-30 17:48:13 -0500

    Base de datos realm, bottom bar y fragments

--- app/src/main/java/maufdh/dev/gradetracking/Activities/MainActivity.java
+++ app/src/main/java/maufdh/dev/gradetracking/Activities/MainActivity.java
@@ -2,6 +2,7 @@ package maufdh.dev.gradetracking.Activities;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.graphics.drawable.ColorDrawable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -33,26 +34,29 @@ public class MainActivity extends AppCompatActivity {
             public void onTabSelected(int tabId) {
                 switch (tabId){
                     case R.id.tab_home:
-                        HomeFragment homeFragment= new HomeFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container,homeFragment)
-                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                                .addToBackStack(null).commit();
+                       changeFragment(new HomeFragment());
+                        changeStatusBarColor(R.color.colorPrimary);
                         break;
                     case R.id.tab_profile:
-                        ProfileFragment profileFragment= new ProfileFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container,profileFragment)
-                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                                .addToBackStack(null).commit();
+                        changeFragment(new ProfileFragment());
+                        changeStatusBarColor(R.color.colorPrimaryDark);
                         break;
                         case R.id.tab_search:
-                        SearchFragment searchFragment= new SearchFragment();
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_container,searchFragment)
-                                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                                .addToBackStack(null).commit();
+                            changeFragment(new SearchFragment());
+                            changeStatusBarColor(R.color.colorAccent);
                         break;
                 }
             }
         });
+    }
+    private void changeFragment(android.support.v4.app.Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_container,fragment)
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                .addToBackStack(null).commit();
+    }
+    private void changeStatusBarColor(int source){
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources()
+                .getColor(source)));
     }
 
 
